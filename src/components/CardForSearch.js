3a3e00deecbe9c967542660dad2e6f3d32bbb33a
EmadIdris <emadidris@outlook.com>
2021-10-05 13:28:58 +0300

    edit

--- src/components/CardForSearch.js
+++ src/components/CardForSearch.js
@@ -3,52 +3,55 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Card, Button } from 'react-bootstrap';
 import CardGroup from 'react-bootstrap/CardGroup'
 // import { Link } from "react-router-dom";
+import { Rating, Typography } from '@mui/material';
+// import Rating from '@mui/material/Rating';
 import axios from 'axios';
 import Modal from 'react-bootstrap/Modal'
+import './styleForModal.css';
 class CardForSearch extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-    this.state={
-            gameProfile:{},
-            showModal:false,
-            requirements:''
+        this.state = {
+            gameProfile: {},
+            showModal: false,
+            requirements: ''
+        }
     }
-}
-    getInfo=()=>{
+    getInfo = () => {
 
-    let gameSlug = this.props.game.slug;
-    let gameUrl = `${process.env.REACT_APP_SERVER}/home/game?gameName=${gameSlug}`
-    axios.get(gameUrl).then(axiosData =>{
-        console.log(gameUrl);
-        console.log(axiosData.data);
-        this.setState({
-            gameProfile:axiosData.data,
-            showModal : true
+        let gameSlug = this.props.game.slug;
+        let gameUrl = `${process.env.REACT_APP_SERVER}/home/game?gameName=${gameSlug}`
+        axios.get(gameUrl).then(axiosData => {
+            console.log(gameUrl);
+            console.log(axiosData.data);
+            this.setState({
+                gameProfile: axiosData.data,
+                showModal: true
+            })
+
+            console.log(axiosData.data.requirements.minimum);
+            if (axiosData.data.requirements.minimum) {
+                this.setState({
+                    requirements: axiosData.data.requirements.minimum
+                })
+            } else if (axiosData.data.requirements.recommended) {
+                this.setState({
+                    requirements: axiosData.data.requirements.recommended
+                })
+            } else {
+                this.setState({
+                    requirements: 'this game has no requirement'
+                })
+            }
+            console.log(this.state.gameProfile);
+            this.props.gHandler(axiosData.data)
         })
 
-        console.log(axiosData.data.requirements.minimum);
-        if(axiosData.data.requirements.minimum){
-            this.setState({
-                requirements:axiosData.data.requirements.minimum
-            })
-        }else if (axiosData.data.requirements.recommended){
-            this.setState({
-                requirements:axiosData.data.requirements.recommended
-            })
-        }else {
-            this.setState({
-                requirements:'this game has no requirement'
-            })
-        }
-        console.log(this.state.gameProfile);
-        this.props.gHandler(axiosData.data)     
-    })
-    
     }
-    
-    closeModel = ()=>{
+
+    closeModel = () => {
         this.setState({
-            showModal:false
+            showModal: false
         })
     }
 
@@ -56,20 +59,22 @@ class CardForSearch extends Component {
     render() {
         return (
             <>
-                <CardGroup style={{ width: '13rem' }}>
-                    <Card style={{ width: '13rem' }}>
-                    <Card.Img variant="top" src={this.props.game.image} />
+                <CardGroup className="cardGame" style={{ width: '20rem' }}>
+                    <Card >
+                        <Card.Img style={{ height: '15rem' }} variant="top" src={this.props.game.image} />
                         <Card.Body>
                             <Card.Title>{this.props.game.name}</Card.Title>
                             <Card.Text>
-                                {this.props.game.rating}
+                                {/* {this.props.game.rating} */}
+                                <Rating name="read-only" value={this.props.game.rating} precision={0.5} readOnly />
+
                             </Card.Text>
                         </Card.Body>
 
-                        <Button variant="primary" onClick={()=>{
+                        <Button variant="primary" onClick={() => {
                             this.props.addGame(this.props.game)
                         }}>Add To Wish List</Button>
-                        <Button onClick={this.getInfo} >More Info</Button>
+                        <Button className="btnCard" onClick={this.getInfo} >More Info</Button>
                     </Card>
                 </CardGroup>
                 <Modal show={this.state.showModal} fullscreen={true} onHide={this.closeModel}>
@@ -77,14 +82,41 @@ class CardForSearch extends Component {
                         <Modal.Title>{this.props.game.name}</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <img src={this.props.game.image}/>
-                        <p>{this.props.game.rating}</p>
-                        <p>{this.state.gameProfile.description}</p>
-                        <p>{this.state.requirements}</p>
+                        {/* <img src={this.props.game.image}/> */}
+                        {/* <p>{this.props.game.rating}</p> */}
+                        {/* <Typography component="legend">Rate</Typography> */}
+                        {/* <Rating name="read-only" value={this.props.game.rating} readOnly /> */}
+                        {/* <p>{this.state.gameProfile.description}</p> */}
+                        {/* <p>{this.state.requirements}</p> */}
+                        <div className="parentDiv">
+                            <div>
+                                <img className="gamePostar" src={this.props.game.image} />
+                                {/* <p>{this.props.home.rating}</p> */}
+                                <div className="rateGame">
+                                    {/* <Typography component="legend">Rate</Typography> */}
+                                    <Rating name="read-only" value={this.props.game.rating} precision={0.5} size="large" readOnly />
+                                </div>
+                            </div>
+                            <div className="paragraphGame">
+                                <div className="storyGame">
+                                    <h2>Story</h2>
+                                    <p >{this.state.gameProfile.description}</p>
+                                </div>
+                                <div className="requirementGame">
+                                    <h2>Requirement</h2>
+                                    <p>{this.state.requirements}</p>
+                                </div>
+                            </div>
+                        </div>
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={this.closeModel}>
                             Close
+                        </Button>
+                        <Button variant="secondary" onClick={() => {
+                            this.props.addGame(this.props.game)
+                        }}>
+                            Add To Wish List
                         </Button>
                     </Modal.Footer>
                 </Modal>
--- src/components/CardForSort.js
+++ src/components/CardForSort.js
@@ -4,14 +4,16 @@ import { Card, Button } from 'react-bootstrap';
 import CardGroup from 'react-bootstrap/CardGroup'
 // import { Link } from "react-router-dom";
 import axios from 'axios';
+import { Rating , Typography } from '@mui/material';
 import Modal from 'react-bootstrap/Modal'
+import './styleForModal.css';
 class CardForSort extends Component {
     constructor(props){
         super(props);
         this.state={
             gameProfile:{},
             showModal:false,
-            
+            requirements:''
         }
     }
     
@@ -54,20 +56,21 @@ class CardForSort extends Component {
     render() {
         return (
             <>
-                <CardGroup style={{ width: '13rem' }}>
+                <CardGroup className="cardGame" style={{ width: '20rem' }}>
                     <Card>
-                    <Card.Img  variant="top" src={this.props.sort.image} />
+                    <Card.Img style={{ height: '15rem'  }}  variant="top" src={this.props.sort.image} />
                         <Card.Body>
                             <Card.Title>{this.props.sort.name}</Card.Title>
                             <Card.Text>
-                                {this.props.sort.rating}
+                                {/* {this.props.sort.rating} */}
+                                <Rating name="read-only" value={this.props.sort.rating}  precision={0.5}  readOnly />
                             </Card.Text>
                         </Card.Body>
 
                         <Button variant="primary" onClick={() => {
                             this.props.addGame(this.props.sort)
                         }}>Add To Wish List</Button>
-                        <Button onClick={this.getInfo} >More Info</Button>
+                        <Button className="btnCard" onClick={this.getInfo} >More Info</Button>
                     </Card>
                 </CardGroup>
                 <Modal show={this.state.showModal} fullscreen={true} onHide={this.closeModel}>
@@ -75,14 +78,43 @@ class CardForSort extends Component {
                         <Modal.Title>{this.props.sort.name}</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <img src={this.props.sort.image}/>
-                        <p>{this.props.sort.rating}</p>
-                        <p>{this.state.gameProfile.description}</p>
-                        <p>{this.state.gameProfile.requirements}</p>
+                        {/* <img src={this.props.sort.image}/> */}
+                        {/* <p>{this.props.sort.rating}</p> */}
+                        {/* <Typography component="legend">Rate</Typography> */}
+                        {/* <Rating name="read-only" value={this.props.sort.rating} readOnly /> */}
+                        {/* <p>{this.state.gameProfile.description}</p>
+                        <p>{this.state.requirements}</p> */}
+
+                        <div className="parentDiv">
+                        <div>
+                        <img className="gamePostar" src={this.props.sort.image} />
+                        {/* <p>{this.props.home.rating}</p> */}
+                        <div className="rateGame">
+                        {/* <Typography component="legend">Rate</Typography> */}
+                        <Rating name="read-only" value={this.props.sort.rating}  precision={0.5} size="large" readOnly />
+                        </div>
+                        </div>
+                        <div className="paragraphGame">
+                           <div className="storyGame"> 
+                        <h2>Story</h2>
+                        <p >{this.state.gameProfile.description}</p>
+                        </div>
+                        <div className="requirementGame"> 
+                        <h2>Requirement</h2>
+                        <p>{this.state.requirements}</p>
+                        </div>
+                        </div>
+                        </div>
+
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={this.closeModel}>
                             Close
+                        </Button>
+                        <Button variant="secondary" onClick={() => {
+                            this.props.addGame(this.props.sort)
+                        }}>
+                            Add To Wish List
                         </Button>
                     </Modal.Footer>
                 </Modal>
